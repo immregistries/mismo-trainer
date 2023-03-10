35101f3b3419c4aa8b78487daac01d4ccaa78080
Nathan Bunker <nbunker@immregistries.org>
2023-03-10 16:34:11 -0700

    Initial commit

---
+++ src/main/java/org/immregistries/mismo/trainer/App.java
@@ -0,0 +1,13 @@
+package org.immregistries.mismo.trainer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
